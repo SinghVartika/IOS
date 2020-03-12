b70519b9b2b6fc04ca17cbe278fcda0d94c97c19
Vartika Singh <vartika.singh@tothenew.com>
2020-03-12 13:08:34 +0530

    Solution for Nested Controller and Storyboard

--- MyApp/MyApp/AppDelegate.swift
+++ MyApp/MyApp/AppDelegate.swift
@@ -11,8 +11,10 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+//
+//    var window: UIWindow(frame: UIScreen.main.bounds)
+//    window?.rootViewController = UINavigationController(rootViewController : YourTabBarController())
+//    window?.makeKeyAndVisible()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let navigationBarAppearace = UINavigationBar.appearance()
