d1112e1d48ecab9027a85bffb0f152c6faac2974
Vartika Singh <vartika.singh@tothenew.com>
2020-03-30 15:14:37 +0530

    Solution for the UIElements Exersice

---
+++ MyApp/MyApp/NestedCustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NestedCustomCollectionViewCell.swift
+//  MyApp
+//
+//  Created by TTN on 26/03/20.
+//  Copyright © 2020 TTN. All rights reserved.
+//
+
+import UIKit
+
+class NestedCustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
